1402d8d29f0adeb1fbda23bc3fd6af17f7911f68
mathieu goncalves <mathieu.goncalves@innovative-technologies.fr>
2022-07-12 14:46:37 +0200

    Update de panel_bouton.py, ajout d'une troisième PIN

--- panel_bouton.py
+++ panel_bouton.py
@@ -29,9 +29,11 @@ defilement = False;
 positionDefilement = 0;
 decalageAccent=0;
 nombreDeCaracteres=8; # Constante (ici testé avec 8 chars, sur le panneau de test)
-tempsAttente = getConfigLine("temps_attente")
+tempsAttente = int(getConfigLine("temps_attente"))
 position_x=0
 position_y=0
+
+#print (datetime.date.today().strftime("%d/%m/%Y"), datetime.datetime.now().strftime("%H:%M:%S"))
 
 def main():
         global nombreDeCaracteres
@@ -40,50 +42,76 @@ def main():
         global position_y
 
         myPopen = subprocess.Popen([BASH_PATH,'--led-no-hardware-pulse','--led-gpio-mapping=adafruit-hat-pwm','-f /root/spleen/spleen-16x32.bdf','--led-brightness=70','--led-limit-refresh=60','--led-chain=2','--led-parallel=1','--led-cols=64','--led-rows=32','--led-row-addr-type=0','--led-scan-mode=0','--led-pwm-lsb-nanoseconds=130','--led-slowdown-gpio=2','-x 0','-y 0','-C 0,0,255'],stdin = subprocess.PIPE,stdout = subprocess.PIPE, stderr = subprocess.PIPE, encoding = 'utf-8',bufsize=1,universal_newlines=True)
-        print("Subprocess : ouvert\n")
+        print(datetime.date.today().strftime("%d/%m/%Y"), datetime.datetime.now().strftime("%H:%M:%S")," : Subprocess : ouvert\n")
 
-        print("Initialisation des GPIO")
+        print(datetime.date.today().strftime("%d/%m/%Y"), datetime.datetime.now().strftime("%H:%M:%S")," : Initialisation des GPIO")
         PIN_1 = 19
         PIN_2 = 25
+        PIN_3 = 24
+        etat_Precedent = "0"
+        etat_Actuel = "0"
         GPIO.setmode(GPIO.BCM)
+        GPIO.cleanup()
         # Si on décommente ça, la valeur de base sera 1, il faut retirer le courant
-        GPIO.setup(PIN_1, GPIO.IN)
-        GPIO.setup(PIN_2, GPIO.IN)
+        GPIO.setup(PIN_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(PIN_2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(PIN_3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         # Si on décommente ça, la valeur de base sera 0, il faut ajouter du courant
         #GPIO.setup(PIN_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         #GPIO.setup(PIN_2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         time.sleep(1)
 
-        print("Entrée dans la boucle infinie\n")
+        myPopen.stdin.write("C:255,255,0;B:0,0,0;X:0;Y:0;T:<<PRET>>\n\n")
+
+        print(datetime.date.today().strftime("%d/%m/%Y"), datetime.datetime.now().strftime("%H:%M:%S")," : Entrée dans la boucle infinie\n")
         try:
             while True: # 1 = HIGH, 0 = LOW, HIGH = base, LOW = activé
-                #print("Lecture GPIO")
+
                 etat_19 = GPIO.input(PIN_1)
                 etat_25 = GPIO.input(PIN_2)
+                etat_24 = GPIO.input(PIN_3)
 
-                #print("19="+str(etat_19))
-                #print("25="+str(etat_25))
+                print ("PIN19 = "+str(etat_19))
+                print ("PIN25 = "+str(etat_25))
+                print ("PIN24 = "+str(etat_24))
 
-                #print("Les IFS")
-                if etat_19==1 and etat_25==1:
-                    #print("1-1")
-                    myPopen.stdin.write("C:255,0,0;B:0,0,0;X:0;Y:0;T:Rien\n\n")
-                if etat_19==1 and etat_25==0:
-                    #print("1-0")
-                    myPopen.stdin.write("C:255,0,0;B:0,0,0;X:0;Y:0;T:Pin25\n\n")
-                if etat_19==0 and etat_25==1:
-                    #print("0-1")
-                    myPopen.stdin.write("C:255,0,0;B:0,0,0;X:0;Y:0;T:Pin19\n\n")
-                if etat_19==0 and etat_25==0:
-                    #print("0-0")
-                    myPopen.stdin.write("C:255,0,0;B:0,0,0;X:0;Y:0;T:Pin19&25\n\n")
+                if etat_19 == 0:
+                    etat_Actuel="19"
+                elif etat_25 == 0:
+                    etat_Actuel="25"
+                elif etat_24 == 0:
+                    etat_Actuel="24"
+                else:
+                    etat_Actuel="0"
 
-                #print("10sec attente")
-                time.sleep(int(tempsAttente))
-        except:
-            print ("Erreur : ", sys.exc_info()[0])
-            #GPIO.cleanup()
+                if etat_Actuel != etat_Precedent:
+                    if etat_19==0:
+                        print (datetime.date.today().strftime("%d/%m/%Y"), datetime.datetime.now().strftime("%H:%M:%S")," : Nouvel état : contact sur pin 25")
+                        etat_Precedent = "19"
+                        myPopen.stdin.write("C:0,255,0;B:0,0,0;X:0;Y:0;T: Ouvert\n\n")
+                    elif etat_25==0:
+                        print (datetime.date.today().strftime("%d/%m/%Y"), datetime.datetime.now().strftime("%H:%M:%S")," : Nouvel état : contact sur pin 19")
+                        etat_Precedent = "25"
+                        myPopen.stdin.write("C:255,0,0;B:0,0,0;X:8;Y:0;T: Fermé\n\n")
+                    elif etat_24==0:
+                        print (datetime.date.today().strftime("%d/%m/%Y"), datetime.datetime.now().strftime("%H:%M:%S")," : Nouvel état : contact sur pin 24")
+                        etat_Precedent = "24"
+                        myPopen.stdin.write("C:255,0,0;B:0,0,0;X:8;Y:0;T:Complet\n\n")
+                    else:
+                        print (datetime.date.today().strftime("%d/%m/%Y"), datetime.datetime.now().strftime("%H:%M:%S")," : Nouvel état : ANORMAL, AUCUN CONTACT")
+                        etat_Precedent = "0"
+                        myPopen.stdin.write("C:255,255,0;B:0,0,0;X:0;Y:0;T:<<PRET>>\n\n")
+
+                print(datetime.date.today().strftime("%d/%m/%Y"), datetime.datetime.now().strftime("%H:%M:%S"),":",str(tempsAttente)+"sec attente")
+                time.sleep(tempsAttente)
+        except Exception as e:
+            print (datetime.date.today().strftime("%d/%m/%Y"), datetime.datetime.now().strftime("%H:%M:%S")," : ERREUR : ", sys.exc_info()[0])
+            print (datetime.date.today().strftime("%d/%m/%Y"), datetime.datetime.now().strftime("%H:%M:%S"),":",e)
+            GPIO.cleanup()
+        finally:
+            print ("Fin du programme, clean des GPIO")
+            GPIO.cleanup()
 
 print("Pre Main")
 main()
-#GPIO.cleanup()
+GPIO.cleanup()
